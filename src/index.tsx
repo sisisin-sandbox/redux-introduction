6c782a4d81cad8e51c8e62b28e6c6bfc7fc2ed8f
sisisin <azsisisin@gmail.com>
2019-07-21 23:21:43 +0900

    feat: redux without react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-ReactDOM.render(<div>hello</div>, document.querySelector('#root'))
+const initialState = { counter: 0 };
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { counter: state.counter + 1 };
+    default:
+      return state;
+  }
+}
+
+interface State {
+  counter: number;
+}
+
+const store = createStore(counter);
+
+class App extends React.Component<any, State> {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div>count: {this.state.counter}</div>
+        <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>hoge</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
